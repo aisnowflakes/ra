ad516c6526cac5134ee5dd760e98ede096dfe476
aisnowflakes <aisnowflakes@gmail.com>
2025-01-10 11:11:57 -0500

    Update parallelization.py
    changing model name

--- parallelization.py
+++ parallelization.py
@@ -12,7 +12,7 @@ from langchain_openai import ChatOpenAI
 
 from langgraph.graph import StateGraph, START, END
 
-llm = ChatOpenAI(model="gpt-4o", temperature=0) 
+llm = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0) 
 
 class State(TypedDict):
     question: str
